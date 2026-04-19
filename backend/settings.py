57b9de5d067841ac50bd06ebfe46268adebc4826
jlcsanselmo <jlcsanselmo@gmail.com>
2026-04-19 12:31:38 -0300

    configuração do backend para receber o frontend

--- backend/settings.py
+++ backend/settings.py
@@ -117,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #rota de login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #renovar a token
 ]
